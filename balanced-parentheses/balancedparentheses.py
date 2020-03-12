c344ecb3cdb58a69afb8a9642d0ff6ffa9748e95
Yichen Dai <msdaiyichen@gmail.com>
2020-03-12 19:11:19 +0000

    3/12 code challenge completed in 30 mins

--- balanced-parentheses/balancedparentheses.py
+++ balanced-parentheses/balancedparentheses.py
@@ -30,6 +30,31 @@ If you receive a string with no parentheses, consider it balanced::
 def has_balanced_parens(phrase):
     """Does a string have balanced parentheses?"""
 
+    # loop over the string
+    # if see an open paren, add to a list
+    # if see a closing paren, remove the last open paren for balance
+    # meanwhile, if no open paren to remove for balance, return False for unbalance--extra ")"
+    # When loop is done, if there's no open paren left, return True for balance, 
+    # else return False for unbalance--extra "(".
+
+    parens = []
+
+    for i in phrase:
+      if i == "(":
+        parens.append(i)
+
+      if i == ")" and parens != []:
+        parens.pop()
+      # use elif to make sure this condition happens at the same time as above, not after  
+      elif i == ")" and parens == []: 
+        return False
+
+    if parens == []:
+      return True
+
+    else:
+      return False
+
 
 if __name__ == '__main__':
     import doctest
